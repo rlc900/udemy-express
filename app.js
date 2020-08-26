db09172a3b003e331650870a055b74cd6e4ab99f
Rianna <rianna.cleary@gmail.com>
2020-08-26 14:34:14 -0400

    reading from API

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+const fs = require('fs')
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  res.status(200).send('Hello from the server side!')
+// app.get('/', (request, response) => {
+//   response
+//   .status(200)
+//   .json(
+//     {message: 'Hello from the server side!',
+//     app: 'Natours'}
+//   )
+// })
+//
+// app.post('/', (request, response) => {
+//   response.send('You can post to this URL!')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (request, response) => {
+  response.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours //variable tours
+    }
+  })
 })
 
 const port = 3000;
