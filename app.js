0df3964daef8dfe6ab19a82e8d7d0664d46b2cd8
Rianna <rianna.cleary@gmail.com>
2020-08-26 15:34:26 -0400

    created another get request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+// creating middleware: a function that can modify the incoming request data; added to request object
+app.use(express.json());
+
 // app.get('/', (request, response) => {
 //   response
 //   .status(200)
@@ -25,6 +28,46 @@ app.get('/api/v1/tours', (request, response) => {
     data: {
       tours //variable tours
     }
+  });
+});
+
+app.get('/api/v1/tours/:id', (request, response) => {
+  // console.log(request.params);
+  // this will create an array which only contains the element if the comparison ends up being true
+  const id = request.params.id * 1;
+  const tour = tours.find(ele => ele.id === id);
+
+  if (!tour) {
+    return response.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'})
+  }
+
+  response.status(200).json({
+    status: 'success',
+    // results: tours.length,
+    data: {
+      tour //variable tours
+    }
+  });
+});
+
+app.post('/api/v1/tours', (request, response) => {
+  // console.log(request.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({id: newId}, request.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    // 201 === created
+    response.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    })
+
   })
 })
 
