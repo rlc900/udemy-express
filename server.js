de7b4956778ecb20a76c0d8e2da70ebb62b2d6a9
Rianna <rianna.cleary@gmail.com>
2020-09-03 12:16:57 -0400

    created schema and model

--- server.js
+++ server.js
@@ -14,7 +14,26 @@ mongoose.connect(database, {
 })
 .then(() => console.log('DB CONNECTION SUCCESSFUL'));
 
-// console.log(process.env);
+
+// creating schema/specify a schema for data
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price.']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+// creating model
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
