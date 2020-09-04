bed46e68a743b0169224df255cb4da8bd3659cb7
Rianna <rianna.cleary@gmail.com>
2020-09-04 10:58:19 -0400

    added import dev-data and models

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,7 +52,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!'
+      message: err
     })
   }
 };
@@ -80,7 +80,7 @@ exports.updateTour = async (req, res) => {
 exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id)
-    
+
     res.status(204).json({
       status: 'success',
       data: null
--- models/tourModel.js
+++ models/tourModel.js
@@ -4,17 +4,54 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name.'],
-    unique: true
+    required: [true, 'Tour must have a name.'],
+    unique: true,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Tour must have a duration.']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size.']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have a difficulty.']
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price.']
+    required: [true, 'Tour must have a price.']
   },
-  rating: {
+  priceDiscount: Number,
+  ratingsAverage: {
     type: Number,
     default: 4.5
-  }
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a description.']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have a cover image.']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 // creating model
