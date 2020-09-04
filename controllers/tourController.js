a21f4f9f64736408c50a4814c6aef10992a71526
Rianna <rianna.cleary@gmail.com>
2020-09-04 13:03:54 -0400

    added pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -39,6 +39,18 @@ if (req.query.fields) {
   query = query.select('-__v');
 }
 
+  // 4) Pagination
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 100;
+  const skip = (page - 1) * limit;
+
+  query = query.skip(skip).limit(limit)
+
+  if (req.query.page) {
+    const numOfTours = await Tour.countDocuments();
+    if (skip >= numOfTours) throw new Error('This page does not exist.')
+  }
+
     // EXECUTE QUERY
     const tours = await query;
 
